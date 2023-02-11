94b66d818e1420a90a2da075471deb04a3b5ee8e
ToniCanoMar <125144736+ToniCanoMar@users.noreply.github.com>
2023-02-11 18:07:05 +0100

    Create Ejercicio1Tema4.java

---
+++ Ejercicio1Tema4.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+    int numeroIf = 10;
+    
+    if (numeroIf > 0) {
+        System.out.println("El número es positivo");
+    } else if (numeroIf < 0) {
+        System.out.println("El número es negativo");
+    } else {
+        System.out.println("El número es 0");
+    }
+}
